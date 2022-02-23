c2b188147c4b42b6e19c01cebdaa11a55a688cd9
voloshynv <riokin2011@gmail.com>
2022-02-23 21:38:03 +0100

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-          Learn React Test 3
+          Learn React Test 4
         </a>
       </header>
     </div>
